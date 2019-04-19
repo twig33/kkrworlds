eab0b197228c69c2ff8b677fdc5deb26359c5668
twig33 <desyat90000@mail.ru>
2019-04-19 23:35:53 +0300

    test i traengles rotate nemnogo podrugomu

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #define GLEW_STATIC
+#define GLFW_DLL
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -27,6 +29,28 @@ GLfloat vertices2[] = { // Note that we start from 0!
 };
 GLuint indices[] = {
 	0, 1, 2
+};
+
+class model
+{
+	public:
+		glm::vec3 localRot = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 globalRot = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 scale = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::mat4 getMatrix()
+		{
+			glm::mat4 mat = glm::mat4(1.0f);
+			mat = glm::rotate(mat, glm::radians(globalRot[0]), glm::vec3(1.0, 0.0, 0.0));
+			mat = glm::rotate(mat, glm::radians(globalRot[1]), glm::vec3(0.0, 1.0, 0.0));
+			mat = glm::rotate(mat, glm::radians(globalRot[2]), glm::vec3(0.0, 0.0, 1.0));
+			mat = glm::translate(mat, position);
+		 	mat = glm::rotate(mat, glm::radians(localRot[0]), glm::vec3(1.0, 0.0, 0.0));
+			mat = glm::rotate(mat, glm::radians(localRot[1]), glm::vec3(0.0, 1.0, 0.0));
+			mat = glm::rotate(mat, glm::radians(localRot[2]), glm::vec3(0.0, 0.0, 1.0));
+			mat = glm::scale(mat, scale);
+			return mat;
+		}
 };
 
 const char *vertexShaderSource = "#version 330 core\n"
@@ -127,82 +151,61 @@ int main(void)
 	float curroffsetx = 0.01;
 	float offsety = 0.01;
 	float curroffsety = 0.01;
-	
-	glm::mat4 trans = glm::mat4(1.0f);
-	trans = glm::translate(trans, glm::vec3(0.0f, 0.15f, -3.5f));
-	trans = glm::rotate(trans, glm::radians(45.0f), glm::vec3(0.0, 0.0, 1.0));
-	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));  
+	 
 
 //	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 	unsigned int triangles = 200;
-	glm::mat4 transforms[triangles];
-	transforms[0] = trans;
+	model transforms[triangles];
+	transforms[0].localRot = glm::vec3(0.0f, 0.0f, 45.0f);
+	transforms[0].scale = glm::vec3(0.5f,0.5f,0.5f);
+	transforms[0].position = glm::vec3(0.0f,0.0f, -3.5f);
 	/*transforms[1] = glm::mat4(1.0f);
 	transforms[1] = glm::translate(transforms[1], glm::vec3(0.0f, 0.55f, 0.0f));
 	transforms[1] = glm::rotate(transforms[1], glm::radians(45.0f), glm::vec3(0.0, 0.0, 1.0));
 	transforms[1] = glm::scale(transforms[1], glm::vec3(0.5, 0.5, 0.5));*/
 	float currot = 0.0f;
 	float curtransy = 0.0f;
-	for (int i = 1; i < triangles - 1; ++i){
-		transforms[i] = trans;
+	for (int i = 1; i < triangles; ++i){
+		transforms[i] = transforms[0];
 		currot += 10.0f;
-		curtransy += 0.025f;
-		transforms[i] = glm::rotate(transforms[i], glm::radians(currot), glm::vec3(0.0, 0.0, 1.0));
-		transforms[i] = glm::translate(transforms[i], glm::vec3(0.0f, curtransy, 0.0f));
+		curtransy += 0.012f;
+		transforms[i].globalRot = glm::vec3(0.0f, 0.0f, currot);
+		transforms[i].position = glm::vec3(0.0f, curtransy, -3.5f);
+//std::cout << transforms[i].position[1] << "\n";
 	}
 	glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)800/(float)600, 0.1f, 100.0f);
+	float currevenz = -3.5f;
+	float curroddz = -3.5f;
+	float evenzadd = 0.02f;
+	float oddzadd = -0.02f;
 	while(!glfwWindowShouldClose(window))
 	{
 		double timeBefore = glfwGetTime();
 		glfwPollEvents();
-	//	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//calc autism
-
-	/*	glUniform4f(fragmentColorLocation, 1.0f, 0.5f, 0.2f, 1.0f);
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-		//bind texture
-		glBindTexture(GL_TEXTURE_2D, texture);
-		//use shader
-		defaultShader.use();
-		//fucking draw
-        glBindVertexArray(VAO[0]);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-		//calc autism
-
-		glUniform4f(fragmentColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-		//bind texture
-		glBindTexture(GL_TEXTURE_2D, texture);
-		//use shader
-		defaultShader.use();
-		//fucking draw
-		glBindVertexArray(VAO[1]);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);   */
-		float currevenz = 0.7f;
-		float curroddz = 0.0f;
-		float evenzadd = 0.02f;
-		float oddzadd = -0.02f;
+		if (currevenz <= -3.75f || currevenz >= -3.25f){
+			evenzadd *= -1;
+		}
+		if (curroddz <= -3.75f || curroddz >= -3.25f){
+			oddzadd *= -1;
+		}
+		currevenz += evenzadd;
+		curroddz += oddzadd;
 		for (int i = 0; i < triangles; ++i){
+			glm::mat4 model;
 			if (i%2 == 0){
 				glUniform4f(fragmentColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-			
-				//if(currevenz >= 1.5f || currevenz <= -1.5f){
-				//	evenzadd = evenzadd * -1;
-				//}
-				//	currevenz += evenzadd;
-				//transforms[i] = glm::translate(transforms[i], glm::vec3(0.0f, 0.0f, evenzadd));
-				transforms[i] = glm::rotate(transforms[i], glm::radians(5.0f), glm::vec3(0.0, 0.0, 1.0));
+				transforms[i].globalRot += glm::vec3(0.0f, 0.0f, 0.5f);
+				transforms[i].localRot += glm::vec3(0.0f, 0.0f, -1.0f);
+				transforms[i].position += glm::vec3(0.0f, 0.0f, evenzadd);
 			} else {
 				glUniform4f(fragmentColorLocation, 1.0f, 0.5f, 0.2f, 1.0f);
-	
-				//if(curroddz <= -1.5f || curroddz >= 1.5f){
-				//	oddzadd *= -1;
-				//}
-				//			curroddz += oddzadd;
-				//transforms[i] = glm::translate(transforms[i], glm::vec3(0.0f, 0.0f, oddzadd));
-				transforms[i] = glm::rotate(transforms[i], glm::radians(-5.0f), glm::vec3(0.0, 0.0, 1.0));
+				transforms[i].globalRot += glm::vec3(0.0f, 0.0f, -0.5f);
+				transforms[i].localRot += glm::vec3(0.0f, 0.0f, 1.0f);
+				transforms[i].position += glm::vec3(0.0f, 0.0f, oddzadd);
 			}
-			glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transforms[i]));
+			model = transforms[i].getMatrix();
+			glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(model));
 			glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 			//bind texture
 			glBindTexture(GL_TEXTURE_2D, texture);
